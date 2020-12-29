05185ce3014f623d346ab86777026c440db08e57
CaesarDavinci <marioalves.eng@gmail.com>
2020-12-29 14:04:38 -0300

    Spring Security

--- src/main/java/br/com/axis/eventosApp/models/Evento.java
+++ src/main/java/br/com/axis/eventosApp/models/Evento.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-//import javax.validation.constraints.NotBlank;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
